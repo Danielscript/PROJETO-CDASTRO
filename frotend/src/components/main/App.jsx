0d55c190f39c4f365a56a5e35dff7fe74f0b1622
Danielscript <59482759+Danielscript@users.noreply.github.com>
2020-06-30 16:09:37 -0300

    Update App.jsx

--- frotend/src/components/main/App.jsx
+++ frotend/src/components/main/App.jsx
@@ -1,9 +1,9 @@
 import './App.css'
 import React from 'react'
-import Logo from '../components/template/Logo'
-import Nav from '../components/template/Nav'
-import Main from '../components/template/Main'
-import Footer from '../components/template/Footer'
+import Logo from '../template/Logo'
+import Nav from '../template/Nav'
+import Main from '../template/Main'
+import Footer from '../template/Footer'
 
 export default props =>
 <div className="app">
